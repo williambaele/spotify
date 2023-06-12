8225c7265f49ea707d32a2c56e246c0c31858f61
William Baele <baelewilliam1999@gmail.com>
2023-06-12 13:15:21 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
 function App() {
   return (
     <div className="h-screen bg-[#191414]">
       <div className="container mx-auto">
-        <h1 className="text-3xl font-bold underline text-[#1DB954]">Hello world!</h1>{" "}
+        <div className="flex items-center justify-center">
+          <h1 className="text-5xl font-bold text-[#1DB954]">
+            Welcome to Spotify's buddy
+          </h1>
+        </div>
       </div>
     </div>
   );
