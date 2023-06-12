a59b3afeaa8a4081e169f784c1850af4a6af0acf
William Baele <baelewilliam1999@gmail.com>
2023-06-12 13:12:06 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
+    <div className="h-screen bg-[#191414]">
+      <div className="container mx-auto">
+        <h1 className="text-3xl font-bold underline text-[#1DB954]">Hello world!</h1>{" "}
+      </div>
     </div>
   );
 }
