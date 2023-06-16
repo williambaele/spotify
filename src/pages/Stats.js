d17ef7a12ff0a000b9d73095d8374118af7a2fa9
William Baele <baelewilliam1999@gmail.com>
2023-06-16 15:06:46 +0200

    add

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
+import Filters from "../components/Filters";
 
 const Stats = () => {
   return (
     <>
       <div className="h-screen bg-[#121212]">
         <Header/>
+        <Filters/>
 
       </div>
     </>
