f82f9b559c8a8b343380c7d82af5e4431bcc2a01
William Baele <baelewilliam1999@gmail.com>
2023-06-16 15:11:02 +0200

    add

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -4,11 +4,11 @@ import Header from "../components/Header";
 const Stats = () => {
   return (
     <>
-      <div className="h-screen bg-[#121212]">
+      <div className="min-h-screen bg-[#121212]">
         <Header />
         <div className="mx-auto container">
           <div className="grid md:grid-cols-2">
-            <div className="bg-green-400 h-screen">a</div>
+            <div className="bg-green-400 min-h-screen	">a</div>
             <div className="bg-red-400"></div>
           </div>
         </div>
