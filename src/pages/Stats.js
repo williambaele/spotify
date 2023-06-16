c2b1d5ce033bf417eada85173c3c98e5374eb48f
William Baele <baelewilliam1999@gmail.com>
2023-06-16 15:09:01 +0200

    add

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -6,9 +6,13 @@ const Stats = () => {
   return (
     <>
       <div className="h-screen bg-[#121212]">
-        <Header/>
-        <Filters/>
-
+        <Header />
+        <div className="mx-auto container">
+          <div className="grid md:grid-cols-2">
+            <div className="bg-green-400 h-screen">a</div>
+            <div className="bg-red-400"></div>
+          </div>
+        </div>
       </div>
     </>
   );
