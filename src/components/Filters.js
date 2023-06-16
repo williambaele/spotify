fc272fe4545b5dc8af437fc1468b24ba395f04ad
William Baele <baelewilliam1999@gmail.com>
2023-06-16 15:24:39 +0200

    add

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -8,23 +8,23 @@ const Filters = () => {
           <h2 className="text-white text-3xl font-bold">Filter you research</h2>
         </div>
         <div className="col-span-2">
-          <div className="grid-cols-2 grid gap-20 divide-x-2">
+          <div className="grid-cols-2 grid gap-20">
             <div className="flex gap-2">
-              <button className="bg-[#595959] py-2 rounded-lg px-4">
+              <button className="bg-[#595959] py-2 rounded-lg px-4 text-sm">
                 Artists
               </button>
-              <button className="bg-[#595959] p-2 rounded-lg px-4">
+              <button className="bg-[#595959] p-2 rounded-lg px-4 text-sm">
                 Tracks
               </button>
             </div>
             <div className="flex gap-2">
-              <button className="bg-[#595959] py-2 rounded-lg px-4">
+              <button className="bg-[#595959] py-2 rounded-lg px-4 text-sm">
                 Last month
               </button>
-              <button className="bg-[#595959] p-2 rounded-lg px-4">
+              <button className="bg-[#595959] p-2 rounded-lg px-4 text-sm">
                 6 months
               </button>
-              <button className="bg-[#595959] p-2 rounded-lg px-4">
+              <button className="bg-[#595959] p-2 rounded-lg px-4 text-sm">
                 All
               </button>
             </div>
