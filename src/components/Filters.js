38fd960dce25169a2744bdfe338593892ff34582
William Baele <baelewilliam1999@gmail.com>
2023-06-19 14:09:29 +0200

    add

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -14,21 +14,21 @@ const Filters = () => {
         <div className="col-span-3  justify-end flex">
           <div className="grid-cols-2 grid gap-4">
             <div className="flex gap-2">
-              <button className="bg-[#1DB954] py-2 rounded-lg px-4 text-sm hover:bg-[#1DB954]  font-large">
+              <button id="artists" className="bg-[#1DB954] py-2 rounded-lg px-4 text-sm hover:bg-[#1DB954] font-large">
                 Artists
               </button>
-              <button className="bg-[#595959] p-2 rounded-lg px-4 text-sm hover:bg-[#1DB954]  font-large">
+              <button id="tracks" className="bg-[#595959] p-2 rounded-lg px-4 text-sm hover:bg-[#1DB954]  font-large">
                 Tracks
               </button>
             </div>
             <div className="flex gap-2">
-              <button className="bg-[#595959] py-2 rounded-lg px-4 text-sm hover:bg-[#1DB954]  font-large">
+              <button id="1m" className="bg-[#595959] py-2 rounded-lg px-4 text-sm hover:bg-[#1DB954]  font-large">
                 Last month
               </button>
-              <button className="bg-[#1DB954] p-2 rounded-lg px-4 text-sm hover:bg-[#1DB954]  font-large">
+              <button id="6m" className="bg-[#1DB954] p-2 rounded-lg px-4 text-sm hover:bg-[#1DB954]  font-large">
                 6 months
               </button>
-              <button className="bg-[#595959] p-2 rounded-lg px-4 text-sm hover:bg-[#1DB954]  font-large">
+              <button id="all" className="bg-[#595959] p-2 rounded-lg px-4 text-sm hover:bg-[#1DB954]  font-large">
                 All
               </button>
             </div>
