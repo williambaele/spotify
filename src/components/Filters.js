0e58db91b16a869ec51f53e8c9564e669fe77047
William Baele <baelewilliam1999@gmail.com>
2023-06-19 11:10:41 +0200

    add

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -5,7 +5,7 @@ const Filters = () => {
     <div className="container mx-auto py-10">
       <div className="grid md:grid-cols-5 items-center px-2 space-y-6 md:space-y-0">
         <div className="col-span-2">
-          <h2 className="text-white text-3xl font-bold">Filter you research</h2>
+          <h2 className="text-white text-4xl font-bold">Filter you research</h2>
         </div>
         <div className="col-span-3  justify-end flex">
           <div className="grid-cols-2 grid gap-4">
